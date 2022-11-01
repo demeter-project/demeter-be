03d8ff392f5b3062bebd5ed87b0ce9ab5b60430c
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-01 14:51:39 -0400

    add model validations and tests for Plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,9 @@
 class Plant < ApplicationRecord
+  validates_presence_of %i[
+    symbol 
+    scientific_name 
+    common_name 
+    temperature_min 
+    moisture_use]
+  validates_numericality_of :temperature_min
 end
--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
   
+  describe 'relationships' do
+
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:symbol) }
+    it { should validate_presence_of(:scientific_name) }
+    it { should validate_presence_of(:common_name) }
+    it { should validate_presence_of(:temperature_min) }
+    it { should validate_presence_of(:moisture_use) }
+
+    it { should validate_numericality_of(:temperature_min) }
+  end
 end
