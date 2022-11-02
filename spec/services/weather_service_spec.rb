f5d02ba816f906fd8614aa1094cffaaa016b3795
Sid Mann <sjmann2@outlook.com>
2022-11-02 13:54:45 -0500

    Fix bug in test

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WeatherService do
-  it 'returns an HTTP response of the 7 day forecast day and night for the given area' do
-    forecast = WeatherService.get_forecast('60647')
+  it 'returns an HTTP response of the 7 day forecast day and night for the given area', :vcr do
+    forecast = WeatherService.get_forecast('41.83', '-87.68')
 
     expect(forecast).to be_a(Hash)
     expect(forecast[:properties]).to have_key(:periods)
