a85a4eb19e38631ad08a0679a765c4081a83d77c
Sid Mann <sjmann2@outlook.com>
2022-11-02 09:13:26 -0500

    test weather facade

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe WeatherFacade do
-  it 'returns a 7 day forecast' do
+  it 'returns a 7 day forecast', :vcr do
     forecast = WeatherFacade.get_forecast('41.83','-87.68')
 
-    expect(forecast).to be_a()
+    expect(forecast).to be_an(Array)
+    expect(forecast.first).to be_instance_of(Weather)
+    expect(forecast.first.day).to be_a(String)
   end
 end
