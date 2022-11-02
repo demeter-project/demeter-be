b99468c8107f5edc878e0e3a74e1788491274b23
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 11:38:01 -0400

    add date_planted column and change default to nil, change quantity default to nil

--- db/migrate/20221102135609_create_plot_plants.rb
+++ db/migrate/20221102135609_create_plot_plants.rb
@@ -3,7 +3,8 @@ class CreatePlotPlants < ActiveRecord::Migration[5.2]
     create_table :plot_plants do |t|
       t.references :plot, foreign_key: true
       t.references :plant, foreign_key: true
-      t.integer :quantity, default: 0
+      t.integer :quantity, default: nil
+      t.datetime :date_planted, default: nil
       t.timestamps
     end
   end
