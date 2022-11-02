ca74e81b6ab9f1eceffc89b8d04fd91432201734
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 13:50:43 -0400

    add methods #soil_ph_min, #soil_ph_max, #shade_tolerant

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -5,4 +5,28 @@ class Plot < ApplicationRecord
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  def soil_ph_min
+    average(:ph_minimum)
+  end
+
+  def soil_ph_max
+    average(:ph_maximum)
+  end
+
+  def average(attribute)
+    if plants.empty?
+      nil
+    else
+      plants.sum { |plant| plant.send(attribute) } / plants.count
+    end
+  end
+
+  def shade_tolerant
+    if plants.empty?
+      nil
+    else
+      plants.all? { |plant| plant.shade_tolerance == "Tolerant"}
+    end
+  end
 end
