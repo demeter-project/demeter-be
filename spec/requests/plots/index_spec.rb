15101afaca47ae0c16cd842e7a0247701be0a230
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 12:44:12 -0400

    update model tests to reflect relationships

--- spec/requests/plots/index_spec.rb
+++ spec/requests/plots/index_spec.rb
@@ -20,9 +20,14 @@ RSpec.describe 'plots#index' do
       expect(expected_plot).to have_key(:id)
       expect(expected_plot).to have_key(:type)
       expect(expected_plot).to have_key(:attributes)
+      expect(expected_plot[:attributes]).to have_key(:name)
+      expect(expected_plot).to have_key(:relationships)
+      expect(expected_plot[:relationships]).to have_key(:plants)
+      expect(expected_plot[:relationships][:plants]).to have_key(:data)
+
 
       expect(expected_plot[:attributes][:name]).to eq(actual_plot.name)
-      expect(expected_plot[:attributes][:plants]).to eq([])
+      expect(expected_plot[:relationships][:plants][:data]).to eq([])
     end
   end
 end
