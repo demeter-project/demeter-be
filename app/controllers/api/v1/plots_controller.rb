8d157863037e15fde2b679ff07d35e7fdfcc2f5e
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 09:48:04 -0400

    build plots#index

--- app/controllers/api/v1/plots_controller.rb
+++ app/controllers/api/v1/plots_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::PlotsController < ApplicationController
   def index
-
+    garden = Garden.find(params[:garden_id])
+    
+    plots = PlotSerializer.new(garden.plots)
+    render json: plots
   end
 end
--- spec/requests/plots/index_spec.rb
+++ spec/requests/plots/index_spec.rb
@@ -3,10 +3,9 @@ require 'rails_helper'
 RSpec.describe 'plots#index' do
   describe 'GET api/v1/gardens/:garden_id/plots' do
     it 'returns a json response with information about all plots in the garden' do
-      get '/api/v1/gardens/:garden_id/plots'
       garden = create :garden
       plots = create_list(:plot, 5, garden: garden)
-      require 'pry'; binding.pry
+      get "/api/v1/gardens/#{garden.id}/plots"
       expect(response).to be_successful
       expect(response).to have_http_status(200)
 
