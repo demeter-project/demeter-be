b16feb314e376cd860575034a845b9b2e5b88d86
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 08:27:46 -0400

    add tested validations and relationship to Plot

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,3 +1,6 @@
 class Plot < ApplicationRecord
   belongs_to :garden
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Plot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :garden }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+  end
 end
