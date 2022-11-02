3a2ad19acf2b9ea23b92bb4859e3ada572aaa77a
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 13:50:43 -0400

    add methods #soil_ph_min, #soil_ph_max, #shade_tolerant

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -11,4 +11,51 @@ RSpec.describe Plot, type: :model do
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of :name }
   end
+
+  describe 'instance methods' do
+    let!(:garden) { create :garden }
+    let!(:plot) { create :plot, garden: garden }
+
+    describe 'ph averages' do
+      let!(:plant_1) { create(:plant, ph_minimum: 4, ph_maximum: 6) }
+      let!(:plant_2) { create(:plant, ph_minimum: 4.5, ph_maximum: 6.5) }
+      let!(:plant_3) { create(:plant, ph_minimum: 5, ph_maximum: 7) }
+      let!(:plants) { [plant_1, plant_2, plant_3] }
+
+      it 'returns nil if plot has no plants' do
+        expect(plot.soil_ph_min).to eq(nil)
+        expect(plot.soil_ph_max).to eq(nil)
+      end
+
+      it 'returns the average of all plants ph_minimum' do
+        plot.plants << plants
+        expect(plot.soil_ph_min).to eq(4.5)
+        expect(plot.soil_ph_max).to eq(6.5)
+      end
+
+      describe '#shade_tolerant' do
+        it 'returns nil if plot has no plants' do
+          expect(plot.shade_tolerant).to eq(nil)
+        end
+        
+        it 'returns a boolean true if all plants are shade tolerant' do
+          plant_1 = create(:plant, shade_tolerance: "Tolerant")
+          plant_2 = create(:plant, shade_tolerance: "Tolerant")
+          plant_3 = create(:plant, shade_tolerance: "Tolerant")
+          plants = [plant_1, plant_2, plant_3]
+          plot.plants << plants
+          expect(plot.shade_tolerant).to eq(true)
+        end
+
+        it 'returns a boolean false if any plants are not shade tolerant' do
+          plant_1 = create(:plant, shade_tolerance: "Tolerant")
+          plant_2 = create(:plant, shade_tolerance: "Intermediate")
+          plant_3 = create(:plant, shade_tolerance: "Intolerant")
+          plants = [plant_1, plant_2, plant_3]
+          plot.plants << plants
+          expect(plot.shade_tolerant).to eq(false)
+        end
+      end
+    end
+  end
 end
