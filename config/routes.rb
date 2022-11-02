74b63ec9df853670e0feded76537e6c19dc9b545
Sid Mann <sjmann2@outlook.com>
2022-11-02 14:39:18 -0500

    Add garden show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :weather, only: [:index]
-      resources :gardens, only: %i(index) do
+      resources :gardens, only: %i(index show) do
         resources :plots, only: %i(index show)
       end
       resources :plants, only: %i[index]  
