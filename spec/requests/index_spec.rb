fa2f6a07f2000d2048416421f0a0f5f67969bc67
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-02 08:22:08 -0400

    generate placeholder models for garden and plot

---
+++ spec/requests/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'plots#index' do
+  describe 'GET api/v1/gardens/:garden_id/plots' do
+    it 'returns a json response with information about all plots in the garden' do
+      get '/api/v1/gardens/:garden_id/plots'
+      garden = create :garden
+      
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      result = JSON.parse(response.body)
+      expect(result).to have_key(:data)
+
+      plots = result[:data]
+      end
+    end
+  end
+end
