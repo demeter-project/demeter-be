5446ae890e9d43ba7d7c25af6314e2dde2a6c7e5
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-01 22:31:42 -0400

    update validations - needs work

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,13 +1,34 @@
 class Plant < ApplicationRecord
-  enum moisture_use: { low: 0, medium: 1, high: 2 }
+  # enum moisture_use: { "Low": 1, "Medium": 2, "High": 3 }
+  # enum drought_tolerance: { "Intolerant": 0, "Low": 1, "Medium": 2, "High": 3 }
+  # enum fire_tolerance: { "Intolerant": 0, "Low": 1, "Medium": 2, "High": 3 }
+  # enum toxicity: { "Nontoxic": 0, "Slight": 1, "Moderate": 2, "Severe": 3 }
+  # enum duration: { "Annual": 0, "Perennial": 1, "Biennial": 2 }
+  # enum growth_rate: { "Slow": 0, "Moderate": 1, "Rapid": 2 }
+  # enum salinity_tolerance: { "Intolerant": 0, "Low": 1, "Medium": 2, "High": 3 }
+  # enum shade_tolerance: { "Intolerant": 0, "Intermediate": 1, "Tolerant": 2 }
 
   validates_presence_of %i[
-    symbol 
+    usda_symbol 
     scientific_name 
     common_name 
-    temperature_min 
+    states
     moisture_use
-    native_states]
+    temperature_min
+  ]
+  # drought_tolerance
+  # fire_tolerance
+  # toxicity
+  # duration
+  # growth_rate
+  # salinity_tolerance
+  # shade_tolerance
+  # ph_minimum
+  # ph_maximum
+  # frost_free_days_min
+  # precipitation_min
+  # precipitation_max
+  # temperature_min 
 
   validates_numericality_of :temperature_min
 
