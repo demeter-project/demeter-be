4760b2979e175e012b10e9c727fed1fd8b54d8b0
Sid Mann <sjmann2@outlook.com>
2022-11-03 07:33:33 -0500

    filter sensitive data vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,7 @@ RSpec.configure do |config|
   VCR.configure do |config|
     config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
+    config.filter_sensitive_data('<location-api>') { ENV['location-api'] }
     config.configure_rspec_metadata!
   end
 end
