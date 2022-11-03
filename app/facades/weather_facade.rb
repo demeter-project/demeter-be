57bd42dbe1af533e7ba14b17a9fc5e60490b42aa
Sid Mann <sjmann2@outlook.com>
2022-11-02 19:07:26 -0500

    adjust accessing lat lon data

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,8 +1,8 @@
 class WeatherFacade
   def self.get_forecast(zipcode)
-    lat_long = HardinessService.get_lat_lon(zipcode)[:coordinates]
-    latitude = lat_long[:lat]
-    longitude = lat_long[:lon]
+    lat_long = LocationService.get_lat_lon(zipcode)[:features][0][:properties]
+    latitude = lat_long[:lat].round(2)
+    longitude = lat_long[:lon].round(2)
     forecast = WeatherService.get_forecast(latitude, longitude)
     forecast[:properties][:periods].map do |day|
       Weather.new(day)
