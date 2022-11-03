d4cd19e466e18486e4a0a454eb6a51cca4789755
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-03 19:55:13 -0400

    remove validations from plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,17 +1,4 @@
-class Plant < ApplicationRecord
-  
-  # validations may not be necessary as we're seeding this data rather than relying on user input
-  validates_presence_of %i[
-    usda_symbol 
-    scientific_name 
-    common_name 
-    states
-    moisture_use
-    temperature_min
-  ]
-  
-  validates_numericality_of :temperature_min
-  
+class Plant < ApplicationRecord  
   has_many :plot_plants
   has_many :plots, through: :plot_plants
   
@@ -19,19 +6,3 @@ class Plant < ApplicationRecord
     where('states like ?', "%#{state}%")
   end
 end
-
-# other attributes that may/may not have data: 
-
-# drought_tolerance
-# fire_tolerance
-# toxicity
-# duration
-# growth_rate
-# salinity_tolerance
-# shade_tolerance
-# ph_minimum
-# ph_maximum
-# frost_free_days_min
-# precipitation_min
-# precipitation_max
-# temperature_min 
