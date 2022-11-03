755d391a47d8477ca433da683ebc15d5f4735d08
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-03 14:30:57 -0400

    add gardens#index action for api/v1/gardens?user_id=x endpoint

--- spec/requests/api/v1/gardens/index_spec.rb
+++ spec/requests/api/v1/gardens/index_spec.rb
@@ -2,10 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'gardens#index' do
   describe 'happy path' do
+    let!(:gardens) { create_list(:garden, 5, user_id: 1, zip_code: "05408") }
 
+    it 'returns a json response with information about all users gardens' do
+      get "/api/v1/gardens?user_id=1"
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result).to have_key(:data)
+      
+      data = result[:data]
+      expect(data.count).to eq(gardens.count)
+
+      first_garden = data.first
+      expect(first_garden).to have_key(:id)
+      expect(first_garden).to have_key(:type)
+      expect(first_garden).to have_key(:attributes)
+
+      attributes = first_garden[:attributes]
+      expect(attributes).to have_key(:name)
+      expect(attributes).to have_key(:zip_code)
+      expect(attributes).to have_key(:state_code)
+      expect(attributes).to have_key(:user_id)
+    end
   end
 
-  describe 'sad path' do
-    
+  describe 'sad path user has no gardens' do
+    it 'returns an empty json and 200 code' do
+      get "/api/v1/gardens?user_id=2"
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result).to have_key(:data)
+      expect(result[:data]).to be_empty
+    end
   end
 end
