a1ac4b828ffebc70e854804e18d268e100dd2e73
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-03 18:21:14 -0400

    restore post update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :gardens, only: %i(index show create) do
-        resources :plots, only: %i(index show create destroy)
+        resources :plots, only: %i(index show create update destroy)
       end
       resources :plants, only: %i[index show]  
     end
