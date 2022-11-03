ac2f04c925db8323c1e19fcc8b14da96c92897e4
Thomas Musselman <tmusselman@gmail.com>
2022-11-03 15:36:22 -0600

    implement route and controller action for updating a plot

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -9,5 +9,9 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
+
+    rescue_from ActionController::ParameterMissing do |e|
+      json_response({ message: e.message }, :bad_request)
+    end
   end
 end
