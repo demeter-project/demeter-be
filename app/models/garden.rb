2824ece4ae55d75403b165bb4e119361010aeb0a
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-03 17:48:56 -0400

    add more robust model validations

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,10 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  validates_presence_of :name, :user_id, :zip_code, :state_code
+
+  validates_numericality_of :user_id
+
+  validates :zip_code, length: { is: 5 }
+  validates :state_code, length: { is: 2 }
 end
