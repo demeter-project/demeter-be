4c02e83c98dbb63c1bb78b879fd9c5e08d325487
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-03 18:41:14 -0400

    add feature: delete endpoint for gardens

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::GardensController < ApplicationController
+  before_action :set_garden, only: %i(show destroy)
+  
   def index
     gardens = Garden.where(user_id: params[:user_id])
     render json: GardenSerializer.new(gardens)
   end
   
   def show
-    garden = Garden.find(params[:id])
-    render json: GardenSerializer.new(garden, params: {weather: true})
+    render json: GardenSerializer.new(@garden, params: {weather: true})
+  end
+
+  def destroy
+    @garden.destroy
+  end
+
+  private
+
+  def set_garden
+    @garden = Garden.find(params[:id])
   end
 end
