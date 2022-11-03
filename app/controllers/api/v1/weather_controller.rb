e9ef4c97e9b272219fa54ffc2f89060f453cf9cf
Sid Mann <sjmann2@outlook.com>
2022-11-02 20:47:49 -0500

    Remove weather serializer, controller, and request spec

--- app/controllers/api/v1/weather_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::WeatherController < ApplicationController
-  def index
-    forecast = WeatherFacade.get_forecast(params[:zipcode])
-    render json: WeatherSerializer.new(forecast)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :weather, only: [:index]
       resources :gardens, only: %i(index show) do
         resources :plots, only: %i(index show)
       end
