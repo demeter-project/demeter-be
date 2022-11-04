d233a71a1953b37a9ee48ee46d3543e701452c87
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-04 08:35:41 -0400

    add hz_range_high class method

--- app/facades/plant_facade.rb
+++ app/facades/plant_facade.rb
@@ -3,6 +3,6 @@ class PlantFacade
     phz_hash = HardinessService.get_hardiness_info(zip_code)
     return nil if phz_hash.nil?
     temp_range = phz_hash[:temperature_range]
-    temp_range.split(' ')[0].to_i
+    temp_range.split(' ').last.to_i
   end
 end
