bf203021d82d5f81ef49f74e6c80a183fd478f08
Sid Mann <sjmann2@outlook.com>
2022-11-03 20:14:13 -0500

    test/feat find plot plants

--- app/models/plot_plant.rb
+++ app/models/plot_plant.rb
@@ -1,14 +1,17 @@
 class PlotPlant < ApplicationRecord
   belongs_to :plot
   belongs_to :plant
+  validates_presence_of :quantity, :plot_id, :plant_id
 
   def plant_name
     plant.name
   end
 
-  def plot_name
-    
+  # def plot_name
+  #   plot.name
+  # end
+
+  def self.find_plot_plants(plot_id)
+    where(plot_id: plot_id)
   end
-
-
 end
--- spec/models/plot_plant_spec.rb
+++ spec/models/plot_plant_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe PlotPlant, type: :model do
     it { should validate_presence_of :plot_id }
     it { should validate_presence_of :plant_id }
   end
+
+  describe 'class methods' do
+    describe 'find plot plants' do
+      it 'returns all plot plants with a matching plot id' do
+        garden = create(:garden)
+        plot_1 = create(:plot, garden: garden)
+        plot_2 = create(:plot, garden: garden)
+
+        plant_1 = create(:plant)
+        plant_2 = create(:plant)
+
+        plot_plant_1 = PlotPlant.create!(plot: plot_1, plant_id: plant_1.id, quantity: 4)
+        plot_plant_2 = PlotPlant.create!(plot: plot_1, plant: plant_2, quantity: 4)
+        plot_plant_3 = PlotPlant.create!(plot: plot_2, plant: plant_2, quantity: 4)
+
+        expect(PlotPlant.find_plot_plants(plot_1.id)).to eq([plot_plant_1, plot_plant_2])
+      end
+    end
+  end
 end
