a6168f00281947cb6f7a8608ef59dd86b8e5d3fb
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-03 20:12:12 -0400

    change state to state_code

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,11 +2,11 @@ class Plant < ApplicationRecord
   has_many :plot_plants
   has_many :plots, through: :plot_plants
   
-  def self.native_to(state)
-    where('states ilike ?', "%#{state}%")
+  def self.native_to(state_code)
+    where('states ilike ?', "%#{state_code}%")
   end
 
-  def self.search_name(name, state)
-    native_to(state).where('common_name ilike ? OR scientific_name ilike ?', "%#{name}%", "%#{name}%")
+  def self.search_name(name, state_code)
+    native_to(state_code).where('common_name ilike ? OR scientific_name ilike ?', "%#{name}%", "%#{name}%")
   end
 end
