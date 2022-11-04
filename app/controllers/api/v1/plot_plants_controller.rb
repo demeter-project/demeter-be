c0e91444a518b7e3a02d1bcb20773c4f184d2212
Sid Mann <sjmann2@outlook.com>
2022-11-04 11:10:54 -0500

    test/feat update a plot plant

--- app/controllers/api/v1/plot_plants_controller.rb
+++ app/controllers/api/v1/plot_plants_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::PlotPlantsController < ApplicationController
     plot_plant = PlotPlant.find_plot_plants(params[:plot_id])
     render json: PlotPlantSerializer.new(plot_plant)
   end
+
+  def update
+    plot_plant = PlotPlant.find(params[:id])
+    plot_plant.update(plot_plant_params)
+    render json: PlotPlantSerializer.new(plot_plant)
+  end
+
+  private
+
+  def plot_plant_params
+    params.require(:plot_plant).permit(:quantity, :date_planted)
+  end
 end
--- spec/requests/api/v1/plot_plants/update_spec.rb
+++ spec/requests/api/v1/plot_plants/update_spec.rb
@@ -1,7 +1,40 @@
-# patch /gardens/:garden_id/plots/:plot_id/plot_plant/:id
+require 'rails_helper'
 
-# This endpoint can receive a patch request for a plot_plant to update it with the date_planted and quantity attributes when its being "planted"
+RSpec.describe 'patch /gardens/:garden_id/plots/:plot_id/plot_plant/:id' do
+  describe 'When the record exists' do
+    describe "updating a plot plant when it is being 'planted'" do
+      it 'updates the quantity and date planted attributes' do
+        garden = create(:garden)
+        plot_1 = create(:plot, garden: garden)
 
+        plant_1 = create(:plant)
+        plant_2 = create(:plant)
+
+        plot_plant_1 = PlotPlant.create!(plot: plot_1, plant_id: plant_1.id)
+        plot_plant_2 = PlotPlant.create!(plot: plot_1, plant: plant_2)
+
+        previous_date = {date_planted: plot_plant_1.date_planted}
+        previous_quantity = {quantity: plot_plant_1.quantity}
+        params = {
+            date_planted: DateTime.now,
+            quantity: 15
+          }
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        patch "/api/v1/gardens/#{garden.id}/plots/#{plot_1.id}/plot_plants/#{plot_plant_1.id}", headers: headers, params: JSON.generate({plot_plant: params})
+        plot_plant = PlotPlant.find(plot_plant_1.id)
+
+        expect(response).to be_successful
+        expect(response).to have_http_status(200)
+        expect(plot_plant.date_planted).to_not eq(previous_date)
+        expect(plot_plant.quantity).to_not eq(previous_quantity)
+        result = JSON.parse(response.body, symbolize_names: true)
+        expect(result[:data][:attributes][:quantity]).to eq(params[:quantity])
+        expect(result[:data][:attributes][:date_planted]).to eq(params[:date_planted].strftime("%Y/%m/%d"))
+      end
+    end
+  end
+end
 # expected http request JSON body:
 
 # {
@@ -22,25 +55,3 @@
 #     }
 #   }
 # }
-require 'rails_helper'
-
-RSpec.describe 'patch /gardens/:garden_id/plots/:plot_id/plot_plant/:id' do
-  describe "updating a plot plant when it is being 'planted'" do
-    it 'updates the quantity and date planted attributes' do
-      garden = create(:garden)
-      plot_1 = create(:plot, garden: garden)
-      plot_2 = create(:plot, garden: garden)
-
-      plant_1 = create(:plant)
-      plant_2 = create(:plant)
-
-      plot_plant_1 = PlotPlant.create!(plot: plot_1, plant_id: plant_1.id, quantity: 4)
-      plot_plant_2 = PlotPlant.create!(plot: plot_1, plant: plant_2, quantity: 4)
-
-      patch "/gardens/#{garden.id}/plots/#{plot.id}/plot_plant/#{plot_plant.id}"
-
-
-
-    end
-  end
-end
