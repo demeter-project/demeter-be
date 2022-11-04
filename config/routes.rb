8942e47d032979bf225ec0138ba4f95a8033a1e9
Sid Mann <sjmann2@outlook.com>
2022-11-04 10:25:02 -0500

    test/feat delete a plot plant

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :gardens, only: %i(index show create destroy) do
         resources :plots, only: %i(index show create update destroy) do
-          resources :plot_plants, only: %i(index)
+          resources :plot_plants, only: %i(index destroy)
         end
       end
       resources :plants, only: %i[index show]  
---
+++ spec/requests/api/v1/plot_plants/delete_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'delete /gardens/:garden_id/plots/:plot_id/plot_plant/:id' do
+  describe 'delete a plant from a garden' do
+    it 'deletes the plot plant record from the table' do
+      garden = create(:garden)
+      plot_1 = create(:plot, garden: garden)
+      plot_2 = create(:plot, garden: garden)
+
+      plant_1 = create(:plant)
+      plant_2 = create(:plant)
+
+      plot_plant_1 = PlotPlant.create!(plot: plot_1, plant_id: plant_1.id, quantity: 4, date_planted: DateTime.now - 1.day)
+      plot_plant_2 = PlotPlant.create!(plot: plot_1, plant: plant_2, quantity: 4, date_planted: DateTime.now - 1.day)
+
+      delete "/api/v1/gardens/#{garden.id}/plots/#{plot_1.id}/plot_plants/#{plot_plant_1.id}"
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(204)
+      expect(response.body).to be_empty
+      expect{PlotPlant.find(plot_plant_1.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
