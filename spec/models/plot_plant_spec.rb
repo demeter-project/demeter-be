0c1d8c71629b7dfb39e8d488113a365fca219f2e
Thomas Musselman <tmusselman@gmail.com>
2022-11-03 21:20:08 -0600

    removed quantity validation test

--- spec/models/plot_plant_spec.rb
+++ spec/models/plot_plant_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe PlotPlant, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of :quantity }
     it { should validate_presence_of :plot_id }
     it { should validate_presence_of :plant_id }
   end
