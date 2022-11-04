70da01852f512065515f5c40f522e4528ee6799d
Thomas Musselman <tmusselman@gmail.com>
2022-11-03 21:07:18 -0600

    Update plot_plant.rb

--- app/models/plot_plant.rb
+++ app/models/plot_plant.rb
@@ -3,10 +3,6 @@ class PlotPlant < ApplicationRecord
   belongs_to :plant
   validates_presence_of :quantity, :plot_id, :plant_id
 
-  def plant_name
-    plant.name
-  end
-
   def self.find_plot_plants(plot_id)
     where(plot_id: plot_id)
   end
