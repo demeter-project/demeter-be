981aa6c1f6f1732585492bcdcaa63616039f4d1f
Thomas Musselman <tmusselman@gmail.com>
2022-11-05 17:18:35 -0600

    update bug with trailing zeroes for weather spec

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -5,6 +5,7 @@ class WeatherService
 
   def self.get_forecast(latitude, longitude)
     url = conn.get("/points/#{latitude},#{longitude}")
+    url = conn.get(url.headers[:location]) if url.status == 301
     forecast_url = JSON.parse(url.body, symbolize_names: true)
 
     response = conn.get("#{forecast_url[:properties][:forecast]}")
