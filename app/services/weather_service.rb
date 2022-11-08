9aa019551c8859a128703aedcd6b21e5b56686db
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-08 12:38:59 -0500

    implement low level caching with 12 hour expiration for weather forecasts

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -4,11 +4,13 @@ class WeatherService
   end
 
   def self.get_forecast(latitude, longitude)
-    url = conn.get("/points/#{latitude},#{longitude}")
-    url = conn.get(url.headers[:location]) if url.status == 301
-    forecast_url = JSON.parse(url.body, symbolize_names: true)
+    Rails.cache.fetch("forecast-#{latitude}-#{longitude}", expires_in: 12.hours) do
+      url = conn.get("/points/#{latitude},#{longitude}")
+      url = conn.get(url.headers[:location]) if url.status == 301
+      forecast_url = JSON.parse(url.body, symbolize_names: true)
 
-    response = conn.get("#{forecast_url[:properties][:forecast]}")
-    JSON.parse(response.body, symbolize_names: true)
+      response = conn.get("#{forecast_url[:properties][:forecast]}")
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
