59a54db7567200e0b957d404b81e1132543d1c77
Thomas Musselman <tmusselman@gmail.com>
2022-11-09 11:29:02 -0700

    feat: add zip code validator that uses locaitonservice

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,9 +2,17 @@ class Garden < ApplicationRecord
   has_many :plots, dependent: :destroy
 
   validates_presence_of :name, :user_id, :zip_code, :state_code
-
   validates_numericality_of :user_id
-
   validates :zip_code, length: { is: 5 }, zipcode: {country_code: :us, message: "is not a valid US postal code"}
   validates :state_code, length: { is: 2 }
+  validate :zip_code_can_get_location
+
+  private
+
+  def zip_code_can_get_location
+    response = LocationService.get_lat_lon(zip_code)
+    if response[:features].empty? || zip_code =='99999'
+      errors.add(:zip_code, "cannot be matched to valid US location")
+    end
+  end
 end
