c507157e1a540bf05227d1b387a569d7f99f037c
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-09 13:14:53 -0500

    protect from error by returning nil if no record found

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,11 +1,16 @@
 class WeatherFacade
   def self.get_forecast(zip_code)
-    lat_long = LocationService.get_lat_lon(zip_code)[:features][0][:properties]
-    latitude = lat_long[:lat].round(2)
-    longitude = lat_long[:lon].round(2)
-    forecast = WeatherService.get_forecast(latitude, longitude)
-    forecast[:properties][:periods].map do |day|
-      Weather.new(day)
+    response = LocationService.get_lat_lon(zip_code)
+    if response[:features].empty?
+      return nil
+    else
+      lat_lon = response[:features][0][:properties]
+      latitude = lat_lon[:lat].round(2)
+      longitude = lat_lon[:lon].round(2)
+      forecast = WeatherService.get_forecast(latitude, longitude)
+      forecast[:properties][:periods].map do |day|
+        Weather.new(day)
+      end
     end
   end
 end
