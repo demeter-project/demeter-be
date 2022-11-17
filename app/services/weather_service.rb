d4c833c9ee7cf780f2b00d626b50a2045a8d4e45
Thomas Musselman <tmusselman@gmail.com>
2022-11-16 17:30:44 -0700

    remove commented code

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,20 +1,3 @@
-# class WeatherService 
-#   def self.conn
-#     Faraday.new(url: 'https://api.weather.gov')
-#   end
-
-#   def self.get_forecast(latitude, longitude)
-#     # Rails.cache.fetch("forecast-#{latitude}-#{longitude}", expires_in: 6.hours) do
-#       url = conn.get("/points/#{latitude},#{longitude}")
-#       url = conn.get(url.headers[:location]) if url.status == 301
-#       return nil if url.status == 404
-#       forecast_url = JSON.parse(url.body, symbolize_names: true)
-#       response = conn.get("#{forecast_url[:properties][:forecast]}")
-#       JSON.parse(response.body, symbolize_names: true)
-#     # end
-#   end
-# end
-
 class WeatherService
 
   def self.get_forecast(zip_code)
